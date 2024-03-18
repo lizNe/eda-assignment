4c12af54e72748ba63293043e84b284b64a7dce6
Elizabeth Neary <20071724@mail.wit.ie>
2024-03-18 15:14:05 +0000

    Completed Fan out implementation - added emailing logic

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -22,13 +22,20 @@ export class EDAAppStack extends cdk.Stack {
       publicReadAccess: false,
     });
 
-    // Output
     
   // Integration infrastructure
 
-  const queue = new sqs.Queue(this, "img-created-queue", {
+  const imageProcessQueue = new sqs.Queue(this, "img-created-queue", {
     receiveMessageWaitTime: cdk.Duration.seconds(10),
   });
+
+  const mailerQ = new sqs.Queue(this, "mailer-queue", {
+    receiveMessageWaitTime: cdk.Duration.seconds(10),
+  });
+
+  const newImageTopic = new sns.Topic(this, "NewImageTopic", {
+    displayName: "New Image topic",
+  }); 
 
   // Lambda functions
 
@@ -43,23 +50,54 @@ export class EDAAppStack extends cdk.Stack {
     }
   );
 
+   const mailerFn = new lambdanode.NodejsFunction(this, "mailer-function", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(3),
+      entry: `${__dirname}/../lambdas/mailer.ts`,
+    });
+
  // S3 --> SQS
-  imagesBucket.addEventNotification(
-    s3.EventType.OBJECT_CREATED,
-    new s3n.SqsDestination(queue)
-  );
+ imagesBucket.addEventNotification(
+  s3.EventType.OBJECT_CREATED,
+  new s3n.SnsDestination(newImageTopic)  // Changed
+);
+
+newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
+
+
+
 
  // SQS --> Lambda
-  const newImageEventSource = new events.SqsEventSource(queue, {
+  const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
     batchSize: 5,
     maxBatchingWindow: cdk.Duration.seconds(10),
   });
 
+  const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
+    batchSize: 5,
+    maxBatchingWindow: cdk.Duration.seconds(10),
+  }); 
+
   processImageFn.addEventSource(newImageEventSource);
+  mailerFn.addEventSource(newImageMailEventSource);
+
 
   // Permissions
 
   imagesBucket.grantRead(processImageFn);
+
+  mailerFn.addToRolePolicy(
+    new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "ses:SendEmail",
+        "ses:SendRawEmail",
+        "ses:SendTemplatedEmail",
+      ],
+      resources: ["*"],
+    })
+  );
 
   // Output
   
